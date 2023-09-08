27f19200c45857cb4c25c9ffa59025931b845f26
selena02ch <103003287+selena02ch@users.noreply.github.com>
2023-09-08 19:14:06 -0300

    Add files via upload

---
+++ UDIscrum/calcularpesoluna.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Calcular peso en la luna</h2>
+    <script type="text/javascript">
+        var peso = prompt ("Ingrese el paso a calcular");
+    </script>
+</body>
+</html>
