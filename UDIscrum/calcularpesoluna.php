0d0594cc97fa6807fcc0e4c9d58a90ad10e95c6c
Felipe Fernandez <felipe-fernandez04@live.com>
2023-09-08 19:27:19 -0300

    Mensaje

--- UDIscrum/calcularpesoluna.php
+++ UDIscrum/calcularpesoluna.php
@@ -9,6 +9,11 @@
     <h2>Calcular peso en la luna</h2>
     <script type="text/javascript">
         var peso = prompt ("Ingrese el paso a calcular");
+
+        var pesoenlaluna=peso*1.622*(1/9.81);
+
+        document.write("Su peso en la luna es: "+pesoenlaluna+"kilogramos");
+        
     </script>
 </body>
 </html>
